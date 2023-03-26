671b41e1701976e678ae658d83fa62592fe84da4
Somojit Arambam <somoarambam123@gmail.com>
2023-03-26 22:44:42 +0530

    server loading ui link tag error fixed

--- components/DesktopNav.tsx
+++ components/DesktopNav.tsx
@@ -9,7 +9,7 @@ import {
   PopoverContent,
   useColorModeValue,
 } from "@chakra-ui/react";
-import { Link } from "@chakra-ui/next-js";
+import Link from "next/link";
 
 import { ChevronRightIcon } from "@chakra-ui/icons";
 
@@ -95,20 +95,21 @@ const DesktopNav = () => {
         <Box key={navItem.label}>
           <Popover trigger={"hover"} placement={"bottom-start"}>
             <PopoverTrigger>
-              <Link
-                p={2}
-                href={navItem.href ?? "#"}
-                fontSize={"md"}
-                fontWeight={500}
-                color={linkColor}
-                _hover={{
-                  textDecoration: "none",
-                  color: "brand.900",
-                  fontWeight: "bold",
-                  transition: "all 5ms ease-in-out",
-                }}
-              >
-                {navItem.label}
+              <Link href={navItem.href ?? "#"}>
+                <Text
+                  p={2}
+                  fontSize={"md"}
+                  fontWeight={500}
+                  color={linkColor}
+                  _hover={{
+                    textDecoration: "none",
+                    color: "brand.900",
+                    fontWeight: "bold",
+                    transition: "all 5ms ease-in-out",
+                  }}
+                >
+                  {navItem.label}
+                </Text>
               </Link>
             </PopoverTrigger>
 
@@ -137,37 +138,38 @@ const DesktopNav = () => {
 
 const DesktopSubNav = ({ label, href, subLabel }: NavItem) => {
   return (
-    <Link
-      href={href!}
-      role={"group"}
-      display={"block"}
-      p={2}
-      rounded={"md"}
-      _hover={{ bg: useColorModeValue("purple.50", "gray.900") }}
-    >
-      <Stack direction={"row"} align={"center"}>
-        <Box>
-          <Text
+    <Link href={href!}>
+      <Box
+        role={"group"}
+        display={"block"}
+        p={2}
+        rounded={"md"}
+        _hover={{ bg: useColorModeValue("purple.50", "gray.900") }}
+      >
+        <Stack direction={"row"} align={"center"}>
+          <Box>
+            <Text
+              transition={"all .3s ease"}
+              _groupHover={{ color: "purple.400" }}
+              fontWeight={500}
+            >
+              {label}
+            </Text>
+            <Text fontSize={"sm"}>{subLabel}</Text>
+          </Box>
+          <Flex
             transition={"all .3s ease"}
-            _groupHover={{ color: "purple.400" }}
-            fontWeight={500}
+            transform={"translateX(-10px)"}
+            opacity={0}
+            _groupHover={{ opacity: "100%", transform: "translateX(0)" }}
+            justify={"flex-end"}
+            align={"center"}
+            flex={1}
           >
-            {label}
-          </Text>
-          <Text fontSize={"sm"}>{subLabel}</Text>
-        </Box>
-        <Flex
-          transition={"all .3s ease"}
-          transform={"translateX(-10px)"}
-          opacity={0}
-          _groupHover={{ opacity: "100%", transform: "translateX(0)" }}
-          justify={"flex-end"}
-          align={"center"}
-          flex={1}
-        >
-          <Icon color={"pink.400"} w={5} h={5} as={ChevronRightIcon} />
-        </Flex>
-      </Stack>
+            <Icon color={"pink.400"} w={5} h={5} as={ChevronRightIcon} />
+          </Flex>
+        </Stack>
+      </Box>
     </Link>
   );
 };
