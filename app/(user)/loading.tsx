f5535b1084584e5954027acf6ead71064d880b68
Somojit Arambam <somoarambam123@gmail.com>
2023-03-26 23:04:52 +0530

    homepage loading ui

--- app/(user)/loading.tsx
+++ app/(user)/loading.tsx
@@ -1,17 +1,21 @@
 "use client";
-import { Spinner, Flex } from "@chakra-ui/react";
+// import { Spinner, Flex } from "@chakra-ui/react";
 
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
   return (
-    <Flex w="full" h="3xl" align="center" justify="center">
-      <Spinner
-        thickness="4px"
-        speed="0.65s"
-        emptyColor="gray.200"
-        color="blue.500"
-        size="xl"
-      />
-    </Flex>
+    // <Flex w="full" h="3xl" align="center" justify="center">
+    //   <Spinner
+    //     thickness="4px"
+    //     speed="0.65s"
+    //     emptyColor="gray.200"
+    //     color="blue.500"
+    //     size="xl"
+    //   />
+    // </Flex>
+
+    <div className="w-full h-screen items-center justify-center">
+      Loading...
+    </div>
   );
 }
