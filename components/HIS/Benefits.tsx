5f11b1f10ae94dbb58a82383a52053e51a96fae3
Somojit Arambam <somoarambam123@gmail.com>
2023-04-28 11:30:21 +0530

    padding added for nabh articles

--- components/HIS/Benefits.tsx
+++ components/HIS/Benefits.tsx
@@ -3,19 +3,19 @@ import React from "react";
 
 const Benefits = () => {
   return (
-    <div className="max-w-full h-auto bg-bgPrimary lg:px-20 px-10 py-20">
+    <div className="h-auto max-w-full px-10 py-20 bg-bgPrimary lg:px-20">
       <div className="flex flex-col ">
-        <h1 className="lg:text-base text-sm">BENEFITS</h1>
-        <h1 className="lg:text-3xl text-2xl font-bold text-brand">
+        <h1 className="text-sm lg:text-base">BENEFITS</h1>
+        <h1 className="text-2xl font-bold lg:text-3xl text-brand">
           Key Benefits
         </h1>
       </div>
-      <div className="grid lg:grid-cols-2 grid-cols-1 mt-10 gap-6">
+      <div className="grid grid-cols-1 gap-6 mt-10 lg:grid-cols-2">
         <div className="flex flex-col">
-          <h1 className="lg:text-xl text-lg font-semibold mb-2">
+          <h1 className="mb-2 text-lg font-semibold lg:text-xl">
             Easy to use and customizable
           </h1>
-          <p className="lg:text-base text-sm leading-relaxed text-justify">
+          <p className="text-sm leading-relaxed text-justify lg:text-base">
             Rely HIS is designed to be user-friendly and easy to navigate. The
             software can be easily customized to meet the specific needs of
             healthcare providers, which means they can configure it to work the
@@ -24,20 +24,20 @@ const Benefits = () => {
         </div>
 
         <div className="flex flex-col">
-          <h1 className="lg:text-xl text-lg font-semibold mb-2">
+          <h1 className="mb-2 text-lg font-semibold lg:text-xl">
             Browser-based interface
           </h1>
-          <p className="lg:text-base text-sm leading-relaxed text-justify">
+          <p className="text-sm leading-relaxed text-justify lg:text-base">
             Rely HIS is a browser-based software, which means it can be accessed
             from any device with an internet connection. This feature makes it
             easy to use and increases adoption rates and productivity.
           </p>
         </div>
         <div className="flex flex-col">
-          <h1 className="lg:text-xl text-lg font-semibold mb-2">
+          <h1 className="mb-2 text-lg font-semibold lg:text-xl">
             QR-based patient registration and appointment booking
           </h1>
-          <p className="lg:text-base text-sm leading-relaxed text-justify">
+          <p className="text-sm leading-relaxed text-justify lg:text-base">
             Rely HIS offers a quick and efficient registration and appointment
             booking system that reduces waiting time using an ORS system. This
             feature ensures that patients can easily book appointments without
@@ -46,10 +46,10 @@ const Benefits = () => {
         </div>
 
         <div className="flex flex-col">
-          <h1 className="lg:text-xl text-lg font-semibold mb-2">
-            Integration with other systems
+          <h1 className="mb-2 text-lg font-semibold lg:text-xl">
+            Integration with Lab/Radiology Equipments
           </h1>
-          <p className="lg:text-base text-sm leading-relaxed text-justify">
+          <p className="text-sm leading-relaxed text-justify lg:text-base">
             Rely HIS can be integrated with other healthcare systems like SAP
             ERP, PACS & RIS, and lab equipment & LIS systems. This feature
             ensures seamless data sharing and management across the
@@ -58,10 +58,10 @@ const Benefits = () => {
         </div>
 
         <div className="flex flex-col">
-          <h1 className="lg:text-xl text-lg font-semibold mb-2">
+          <h1 className="mb-2 text-lg font-semibold lg:text-xl">
             Compliance with industry standards
           </h1>
-          <p className="lg:text-base text-sm leading-relaxed text-justify">
+          <p className="text-sm leading-relaxed text-justify lg:text-base">
             Rely HIS is compliant with HL7, DICOM, ICD-10 standards, which
             ensures that it is interoperable with other healthcare systems and
             can seamlessly share data with other providers.
@@ -69,10 +69,10 @@ const Benefits = () => {
         </div>
 
         <div className="flex flex-col">
-          <h1 className="lg:text-xl text-lg font-semibold mb-2">
+          <h1 className="mb-2 text-lg font-semibold lg:text-xl">
             Token/queue system
           </h1>
-          <p className="lg:text-base text-sm leading-relaxed text-justify">
+          <p className="text-sm leading-relaxed text-justify lg:text-base">
             Rely HIS offers a token/queue system that helps healthcare providers
             to manage patient flow and reduce wait times. This feature ensures
             that patients are seen in a timely and efficient manner.
@@ -80,10 +80,10 @@ const Benefits = () => {
         </div>
 
         <div className="flex flex-col">
-          <h1 className="lg:text-xl text-lg font-semibold mb-2">
+          <h1 className="mb-2 text-lg font-semibold lg:text-xl">
             Mobile apps for doctors, nurses, and patients
           </h1>
-          <p className="lg:text-base text-sm leading-relaxed text-justify">
+          <p className="text-sm leading-relaxed text-justify lg:text-base">
             Rely HIS offers mobile apps for doctors, nurses, and patients, which
             enables them to access the system remotely and stay connected with
             the hospital from anywhere. This feature ensures that medical
@@ -93,10 +93,10 @@ const Benefits = () => {
         </div>
 
         <div className="flex flex-col">
-          <h1 className="lg:text-xl text-lg font-semibold mb-2">
+          <h1 className="mb-2 text-lg font-semibold lg:text-xl">
             Integration with other systems
           </h1>
-          <p className="lg:text-base text-sm leading-relaxed text-justify">
+          <p className="text-sm leading-relaxed text-justify lg:text-base">
             Rely HIS supports barcode, SMS integration, and third-party
             integration with Whatsapp, payment gateways, and other systems. This
             feature ensures that healthcare providers can use the software with
@@ -105,10 +105,10 @@ const Benefits = () => {
         </div>
 
         <div className="flex flex-col">
-          <h1 className="lg:text-xl text-lg font-semibold mb-2">
+          <h1 className="mb-2 text-lg font-semibold lg:text-xl">
             Certifications
           </h1>
-          <p className="lg:text-base text-sm leading-relaxed text-justify">
+          <p className="text-sm leading-relaxed text-justify lg:text-base">
             Rely HIS is CERT-IN certified by the Ministry of Electronics and
             Information Technology, Government of India, which ensures that it
             is secure and meets industry standards. Additionally, it is
@@ -118,10 +118,10 @@ const Benefits = () => {
         </div>
 
         <div className="flex flex-col">
-          <h1 className="lg:text-xl text-lg font-semibold mb-2">
+          <h1 className="mb-2 text-lg font-semibold lg:text-xl">
             Successful implementation
           </h1>
-          <p className="lg:text-base text-sm leading-relaxed text-justify">
+          <p className="text-sm leading-relaxed text-justify lg:text-base">
             Rely HIS is running successfully in more than 30 hospitals ranging
             from 50 to 500+ bedded hospitals. It supports multi-center/chain
             hospitals and can be hosted on-premise/MeitY certified cloud. The
--- components/NABH/Description.tsx
+++ components/NABH/Description.tsx
@@ -7,7 +7,7 @@ const Description = ({ dataContent, eventImage }: any) => {
   const parsed = parse(dataContent);
 
   return (
-    <div className="flex flex-col items-center w-full h-auto space-x-5 lg:items-start lg:flex-row">
+    <div className="flex flex-col items-center w-full h-auto px-2 space-x-5 overflow-x-scroll lg:px-0 lg:items-start lg:flex-row">
       <div className="relative flex-shrink-0 w-56 h-56">
         <Image
           src={`http://prewebsite.relyhealthtech.com/gallery/event/${eventImage}`}
@@ -16,7 +16,7 @@ const Description = ({ dataContent, eventImage }: any) => {
           style={{ objectFit: "fill" }}
         />
       </div>
-      <div className="flex flex-col">{parsed}</div>
+      <div className="flex flex-col w-full ">{parsed}</div>
     </div>
   );
 };
