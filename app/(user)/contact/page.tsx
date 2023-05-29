e5d4c32368878f00bec61fc817fe00b20307c227
Somojit Arambam <somoarambam123@gmail.com>
2023-05-29 15:35:10 +0530

    contact form linked with sanity

--- app/(user)/contact/page.tsx
+++ app/(user)/contact/page.tsx
@@ -30,7 +30,7 @@ const ContactPage = async () => {
         <Flex maxWidth={"7xl"} marginX="auto" direction={"column"} marginBottom={20} paddingX={10}>
             <h1 className={"text-3xl font-medium tracking-wide text-textPrimary"}>Career</h1>
             <p className={"text-base mt-5 leading-loose"} >
-                Discover exciting career opportunities at Rely HealthTech, where we&apos;re shaping the future of healthcare. Join our innovative team and make a meaningful impact.<br /> Email your resume to <Link className={"text-textPrimary font-semibold"} href={`mailto:hr@relyhealthtech.com`}>
+                Discover exciting career opportunities at Rely Healthcare Technologies, where we&apos;re shaping the future of healthcare. Join our innovative team and make a meaningful impact.<br /> Email your resume to <Link className={"text-textPrimary font-semibold"} href={`mailto:hr@relyhealthtech.com`}>
             hr@relyhealthtech.com
           </Link> and start your journey with us today.
             </p>
