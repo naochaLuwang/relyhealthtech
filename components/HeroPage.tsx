4e158ae9e953627efc7775c21025bf1ac329efc6
Somojit Arambam <somoarambam123@gmail.com>
2023-06-20 17:52:15 +0530

    First Commit

--- components/HeroPage.tsx
+++ components/HeroPage.tsx
@@ -84,7 +84,7 @@ const HeroPage = () => {
     <div className="w-full h-auto py-20 lg:py-10">
       <div className="grid w-full h-auto grid-cols-1 mx-auto lg:grid-cols-2 max-w-7xl">
         <div className="relative w-full h-auto ">
-          <svg
+          {/* <svg
             viewBox="0 0 200 200"
             xmlns="http://www.w3.org/2000/svg"
             className="absolute -top-5 -z-30 w-96 h-96 bg-opacity-10 text-opacity-10"
@@ -94,7 +94,7 @@ const HeroPage = () => {
               d="M52.9,-52.1C68.7,-37.2,81.6,-18.6,83.3,1.8C85.1,22.1,75.7,44.2,60,57.2C44.2,70.2,22.1,74.2,0.6,73.5C-20.8,72.9,-41.7,67.7,-56,54.7C-70.4,41.7,-78.2,20.8,-78.3,-0.1C-78.3,-21,-70.6,-41.9,-56.3,-56.9C-41.9,-71.8,-21,-80.7,-1.2,-79.5C18.6,-78.3,37.2,-67,52.9,-52.1Z"
               transform="translate(100 100)"
             />
-          </svg>
+          </svg> */}
 
           <div className="flex flex-col justify-center w-full h-full pl-4 tracking-wider lg:pl-20">
             <p className="text-4xl font-extrabold tracking-wider lg:text-7xl text-textPrimary ">
