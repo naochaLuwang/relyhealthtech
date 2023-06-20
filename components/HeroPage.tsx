9c9c1a236f0753b4b5a39be7315decbbe263599c
Somojit Arambam <somoarambam123@gmail.com>
2023-06-20 16:52:40 +0530

    First Commit

--- components/HeroPage.tsx
+++ components/HeroPage.tsx
@@ -1,6 +1,85 @@
+"use client";
+import postMessage from "@/lib/postMessage";
+import { Message } from "@/typings";
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  SimpleGrid,
+  Textarea,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
 import Image from "next/image";
+import { useState } from "react";
 
 const HeroPage = () => {
+  const { isOpen, onToggle, onOpen, onClose } = useDisclosure();
+  const [open, setOpen] = useState(false);
+  const [formData, setFormData] = useState<Message>({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const toast = useToast();
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      if (!formData.firstName || !formData.phone || !formData.message) {
+        setLoading(false);
+        toast({
+          title: "Invalid Credentials",
+          description: "Please enter all the required fields",
+          status: "error",
+          duration: 9000,
+          isClosable: true,
+        });
+      } else if (formData.phone.length < 10) {
+        setLoading(false);
+        toast({
+          title: "Invalid Credentials",
+          description: "Please enter valid mobile number",
+          status: "error",
+          duration: 9000,
+          isClosable: true,
+        });
+      } else {
+        setLoading(true);
+        postMessage(formData, setFormData, toast, setLoading);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onOpenClick = () => {
+    setOpen(!open);
+  };
+
+  const onCloseClick = () => {
+    setOpen(false);
+  };
   return (
     <div className="w-full h-auto py-20 lg:py-10">
       <div className="grid w-full h-auto grid-cols-1 mx-auto lg:grid-cols-2 max-w-7xl">
@@ -30,9 +109,85 @@ const HeroPage = () => {
             <p className="text-4xl font-extrabold tracking-widest text-textGreen">
               HEALTHCARE IT
             </p>
-            <div className="bg-gradient-to-b from-[#297d05] to-[#6ebf4b]  hover:from-[#6ebf4b] hover:to-[#297d05] transition-all duration-200 ease-in-out lg:text-base text-xs  w-fit px-3 lg:px-4 rounded-md text-buttonColor py-2 mt-12 font-semibold  block ">
+            <div
+              className="bg-gradient-to-b from-[#297d05] to-[#6ebf4b]  hover:from-[#6ebf4b] hover:to-[#297d05] transition-all duration-200 ease-in-out lg:text-base text-xs  w-fit px-3 lg:px-4 rounded-md text-buttonColor py-2 mt-12 font-semibold  block cursor-pointer"
+              onClick={onOpenClick}
+            >
               SCHEDULE A DEMO
             </div>
+
+            <Modal
+              isOpen={open}
+              onClose={onCloseClick}
+              isCentered
+              size={{ base: "sm", lg: "4xl" }}
+            >
+              <ModalOverlay>
+                <ModalContent>
+                  <ModalHeader px={10}>Book a Demo</ModalHeader>
+                  <ModalCloseButton />
+                  <ModalBody px={10}>
+                    <form onSubmit={handleSubmit}>
+                      <SimpleGrid w="full" columns={{ base: 1, lg: 2 }} gap={5}>
+                        <FormControl isRequired>
+                          <FormLabel>First Name</FormLabel>
+                          <Input
+                            type={"text"}
+                            name="firstName"
+                            onChange={handleInputChange}
+                            value={formData.firstName}
+                          />
+                        </FormControl>
+                        <FormControl>
+                          <FormLabel>Last Name</FormLabel>
+                          <Input
+                            type={"text"}
+                            name="lastName"
+                            onChange={handleInputChange}
+                            value={formData.lastName}
+                          />
+                        </FormControl>
+                        <FormControl>
+                          <FormLabel>Email</FormLabel>
+                          <Input
+                            type={"email"}
+                            name="email"
+                            onChange={handleInputChange}
+                            value={formData.email}
+                          />
+                        </FormControl>
+                        <FormControl isRequired>
+                          <FormLabel>Mobile No.</FormLabel>
+                          <Input
+                            type={"text"}
+                            name="phone"
+                            onChange={handleInputChange}
+                            value={formData.phone}
+                            rounded="lg"
+                            maxLength={15}
+                          />
+                        </FormControl>
+                      </SimpleGrid>
+                      <FormControl isRequired mt="5">
+                        <FormLabel>Message</FormLabel>
+                        <Textarea
+                          noOfLines={3}
+                          name="message"
+                          onChange={handleInputChange}
+                          value={formData.message}
+                        ></Textarea>
+                      </FormControl>
+                      <button
+                        className="w-full px-4 py-2 mt-5 mb-10 font-semibold rounded-md lg:w-fit bg-whatsNew text-buttonColor"
+                        type="submit"
+                      >
+                        {loading ? "Submitting..." : "Submit"}
+                      </button>
+                    </form>
+                  </ModalBody>
+                </ModalContent>
+              </ModalOverlay>
+            </Modal>
           </div>
         </div>
         <div className="relative flex-1 w-full lg:h-[28rem] h-96">
