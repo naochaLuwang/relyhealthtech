6a3b25822684adb17d86265c2076a189e1cdf5de
Somojit Arambam <somoarambam123@gmail.com>
2023-06-22 10:46:13 +0530

    First Commit

--- components/Statistics.tsx
+++ components/Statistics.tsx
@@ -28,8 +28,6 @@ function StatsCard(props: StatsCardProps) {
       border={{ base: "1px solid", lg: "0px" }}
       borderColor={"brand.900"}
       rounded={{ base: "lg", lg: "none" }}
-      maxW={{ xl: "7xl" }}
-      mx={"auto"}
     >
       <Flex justifyContent={"start"}>
         <Box
